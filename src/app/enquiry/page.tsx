47727e94a37dc6ff05b0b181637668593bf700ba
Satya Rajbhar <bhaisatya067@gmail.com>
2025-04-25 19:33:35 +0530

    error solved-2

--- src/app/enquiry/page.tsx
+++ src/app/enquiry/page.tsx
@@ -64,7 +64,7 @@ export default function EnquiryPage() {
       } else {
         setMessage("Failed to send email.");
       }
-    } catch (error) {
+    } catch (_error) {
       setMessage("Error sending email.");
     } finally {
       setLoading(false);
